3eaf5e321fa4d69af2bbbe4fefff29a86d0d67ee
Bill Watters <billywatters13@gmail.com>
2020-10-15 01:42:48 +0000

    Added safe parameters

--- teachers.js
+++ teachers.js
@@ -6,3 +6,23 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 }); 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5; 
+pool.query(queryString, values);
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher
+LIMIT $2;
+`)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+});
